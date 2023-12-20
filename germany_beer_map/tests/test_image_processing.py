18a828d99632750c305a4e5c1ea88ee3f15eeef7
daniel-jakob <daniel@jakob.ie>
2023-12-20 15:10:01 +0000

    Refactor test_detect_circles to use pytest fixtures and compare results with reference image

--- germany_beer_map/tests/test_image_processing.py
+++ germany_beer_map/tests/test_image_processing.py
@@ -1,11 +1,19 @@
-import numpy as np
+import pytest
+import os
 from germany_beer_map.src.image_processing.circle_detection import detect_circles
 
-def test_detect_circles():
-    # Assuming 'processed_image.txt' is a file containing a processed image
-    # in the form of a numpy array saved as text
-    processed_image_filename = 'germany_beer_map/tests/fixtures/out_binary.jpg'
-    detected_circles = detect_circles(processed_image_filename)
+@pytest.fixture(params=['fixtures/binary_out.jpg', 'fixtures/binary_out.txt'])
+def processed_image_path(request):
+    return os.path.join(os.path.dirname(__file__), request.param)
+
+
+def test_detect_circles(processed_image_path):
+    detected_circles = detect_circles(processed_image_path)
+
+    # Assuming both .jpg and .txt files should have the same number of circles
+    reference_image_path = os.path.join(os.path.dirname(__file__), 'fixtures/binary_out.jpg')
+    reference_circles = detect_circles(reference_image_path)
 
     assert detected_circles is not None
+    assert len(detected_circles) == len(reference_circles)
     assert len(detected_circles) >= 2
