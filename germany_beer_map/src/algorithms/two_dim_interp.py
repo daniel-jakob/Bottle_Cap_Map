f0e99c55398e0c143b13a60fd2ee123b332e2efd
daniel-jakob <daniel@jakob.ie>
2023-12-24 13:09:42 +0000

    Refactor two_dim_interp function to return circles_interp as a single array

--- germany_beer_map/src/algorithms/two_dim_interp.py
+++ germany_beer_map/src/algorithms/two_dim_interp.py
@@ -21,8 +21,8 @@ def two_dim_interp(centres_of_holes, plotting=False, ref_contour=None, mapping="
 
 	distinctive_points_x = mapping[:, 0]
 	distinctive_points_y = mapping[:, 1]
-	distinctive_points_longitude = mapping[:, 3]
 	distinctive_points_latitude = mapping[:, 2]
+	distinctive_points_longitude = mapping[:, 3]
 
 	circles_interp_longitude = griddata(
 		(distinctive_points_x, distinctive_points_y),
@@ -39,6 +39,8 @@ def two_dim_interp(centres_of_holes, plotting=False, ref_contour=None, mapping="
 	)
 
 	# print(circles_interp_latitude)
+
+	circles_interp = np.column_stack((circles_interp_longitude, circles_interp_latitude))
 
 	if plotting:
 		reference_contour_x = ref_contour[:, 0]
@@ -114,4 +116,4 @@ def two_dim_interp(centres_of_holes, plotting=False, ref_contour=None, mapping="
 
 		plt.show()
 
-	return circles_interp_longitude, circles_interp_latitude
+	return circles_interp
