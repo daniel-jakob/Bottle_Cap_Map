46e39e49a9cf542dd93e70613fe47e6ce2ba9f41
daniel-jakob <daniel@jakob.ie>
2023-12-24 16:10:02 +0000

    Add spatial distance calculation and visualization
    This commit adds a new function to calculate the spatial distance between fixed points and movable points using the Haversine formula. It also includes a visualisation function to display the cost matrix and highlight the optimal assignment.

--- germany_beer_map/src/algorithms/spatial_dist_min.py
+++ germany_beer_map/src/algorithms/spatial_dist_min.py
@@ -1,19 +1,42 @@
 import numpy as np
-from scipy.spatial.distance import cdist
 from scipy.optimize import linear_sum_assignment
+from math import radians, sin, cos, sqrt, atan2
+
+import matplotlib.pyplot as plt
+
+def haversine(lat1, lon1, lat2, lon2):
+	# Convert coordinates to radians
+	lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+
+	# Differences
+	dlon = lon2 - lon1
+	dlat = lat2 - lat1
+
+	# Haversine formula
+	a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+	c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+	# Radius of earth in kilometers (mean radius = 6,371km)
+	R = 6371.0
+
+	# Calculate the distance
+	distance = R * c
+
+	return distance
 
 def spatial_dist_min(points_fixed, points_movable):
 	"""
 	spatial_dist_min, calculates the optimal mapping between two sets of geographical points to minimize the total Euclidean distance. The first set of points, referred to as the reference set, represents fixed locations. The second set of points, which will be mapped to the reference set, represents movable locations. The function uses the Hungarian Algorithm to find the optimal one-to-one correspondence between the two sets that results in the smallest cumulative distance. The output is a list of index pairs representing the optimal assignments and the minimum total distance.
 
 	Parameters:
-	points (list): A list of points.
+	points_fixed (list): A list of tuples, where each tuple contains the longitude and latitude of a fixed point (holes in map).
+	points_movable (list): A list of tuples, where each tuple contains the longitude and latitude of a movable point (bottle caps/brewery locations)).
 
 	Returns:
 	float: The minimum spatial distance between the points.
 	"""
 	# Create a cost matrix
-	cost_matrix = cdist(points_fixed, points_movable, metric='euclidean')
+	cost_matrix = [[haversine(lat1, lon1, lat2, lon2) for (lon2, lat2) in points_fixed] for (lon1, lat1) in points_movable]
 
 	# If there are fewer movable points than fixed holes, pad the cost matrix with zeros
 	if len(points_movable) < len(points_fixed):
@@ -23,8 +46,43 @@ def spatial_dist_min(points_fixed, points_movable):
 	# Use the Hungarian Algorithm to find the optimal assignment
 	row_ind, col_ind = linear_sum_assignment(cost_matrix)
 
+	# Convert cost_matrix to numpy array for advanced indexing
+	cost_matrix_np = np.array(cost_matrix)
+
 	# Calculate the minimum total distance
-	min_total_distance = cost_matrix[row_ind, col_ind].sum()
+	min_total_distance = cost_matrix_np[row_ind, col_ind].sum()
 
 	# Return the optimal assignment and the minimum total distance
+	visualize_cost_matrix(cost_matrix_np, row_ind, col_ind)
 	return list(zip(row_ind, col_ind)), min_total_distance
+
+
+def visualize_cost_matrix(cost_matrix, row_ind, col_ind):
+    # Create column and row labels
+    col_labels = ['Movable Point ' + str(i) for i in range(len(cost_matrix[0]))]
+    row_labels = ['Fixed Hole ' + str(i) for i in range(len(cost_matrix))]
+
+    # Convert cost matrix entries to strings with 2 decimal places
+    formatted_values = [["{:.2f}".format(val) for val in row] for row in cost_matrix]
+
+    # Define a scaling factor for the figure size
+    scale = 0.5
+
+    # Create a new figure with width and height set to the corresponding lengths of the table
+    fig, ax = plt.subplots(figsize=(len(col_labels)*scale, len(row_labels)*scale))
+
+    # Hide axes
+    ax.axis('off')
+
+    # Create a table and add it to the figure
+    table = ax.table(cellText=formatted_values, colLabels=col_labels, rowLabels=row_labels, cellLoc='center', loc='center')
+
+    # Auto adjust the width of the columns
+    table.auto_set_column_width(col=list(range(len(col_labels))))
+
+    # Highlight the cells corresponding to the optimal assignment
+    for i, j in zip(row_ind, col_ind):
+        table.get_celld()[(i+1, j)].set_facecolor('lightgreen')
+
+    # Show the table
+    plt.show()
--- germany_beer_map/src/main.py
+++ germany_beer_map/src/main.py
@@ -11,6 +11,7 @@ from algorithms.tps_transform import *
 from algorithms.two_dim_interp import *
 from user_interface.draw_contours import *
 from algorithms.geocode import get_geocoordinates
+from algorithms.spatial_dist_min import spatial_dist_min
 
 # Create a single instance of ImageProcessor
 preprocessor_photo = ImageProcessor("germany_beer_map/data/images/map.jpg")
@@ -55,16 +56,26 @@ print(circles_coords)
 # Read in bottle_caps.csv
 bottle_caps = []
 with open("germany_beer_map/data/mapping/bottle_caps.csv", 'r') as f:
-    reader = csv.reader(f, delimiter=',')
-    next(reader)  # Skip the header
-    for row in reader:
-        bottle_caps.append(row)
+	reader = csv.reader(f, delimiter=',')
+	next(reader)  # Skip the header
+	for row in reader:
+		bottle_caps.append(row)
 
 bottle_cap_coords = []
 for i in range(len(bottle_caps)):
 	bottle_cap = get_geocoordinates(bottle_caps[i][2])
 	bottle_cap_coords.append(bottle_cap)
+	print(bottle_cap)
 print(np.array(bottle_cap_coords))
+
+# bottle_cap_coords = [ [13.0944453, 54.2907393 ], [12.5440824,  50.68292505], [12.5440824,  50.68292505],
+#  [ 7.95650099, 50.9910225 ],
+#  [ 9.653526,   51.4176975 ],
+#  [11.55363663, 48.14608575],
+#  [11.9069408,  48.3068168 ],
+#  [16.36783075, 48.21118145]]
+
+spatial_dist_min(bottle_cap_coords, circles_coords)
 exit(0)
 
 # draw_contours(ref_contour_scaled_aligned)
