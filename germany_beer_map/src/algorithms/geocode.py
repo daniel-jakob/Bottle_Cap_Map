495851c40e65f762cfd6b65a88586cef26bd6f02
daniel-jakob <daniel@jakob.ie>
2024-01-10 22:16:19 +0000

    Refactor adrress to coordinate conversion

--- germany_beer_map/src/algorithms/geocode.py
+++ germany_beer_map/src/algorithms/geocode.py
@@ -1,4 +1,5 @@
 import requests
+import numpy as np
 
 def get_geocoordinates(address):
     """
@@ -21,3 +22,18 @@ def get_geocoordinates(address):
         return float(data[0]['lon']), float(data[0]['lat'])
 
     return None
+
+def convert_address_to_coords(csvfile):
+    # Read in bottle_caps.csv
+	bottle_caps = np.genfromtxt(csvfile, delimiter=",", dtype=str, skip_header=1)
+
+	bottle_cap_coords = []
+
+	# bottle_cap_coords = [(13.0944453, 54.2907393), (11.591350844509808, 53.37741685), (12.459549961499999, 50.52140085), (7.956500987110655, 50.9910225), (9.653526, 51.4176975), (11.553636633022338, 48.14608575), (11.9069408, 48.3068168), (6.654346047066047, 51.83582915), (10.8870087, 49.889238199999994), (10.227481824523348, 50.45433915), (11.606263141955619, 50.1081385), (7.1418535749854755, 51.26109235)]
+
+	# Get the geocoordinates of each bottle cap
+	for i in range(len(bottle_caps)):
+		bottle_cap = get_geocoordinates(bottle_caps[i][2])
+		bottle_cap_coords.append(bottle_cap)
+
+	return bottle_cap_coords
--- germany_beer_map/src/main.py
+++ germany_beer_map/src/main.py
@@ -1,6 +1,5 @@
 # main.py
 import cv2
-import csv
 
 from image_processing.image_preprocessor import ImageProcessor
 
@@ -10,13 +9,12 @@ from algorithms.scaling import *
 from algorithms.tps_transform import *
 from algorithms.two_dim_interp import *
 from user_interface.draw_contours import *
-from algorithms.geocode import get_geocoordinates
+from algorithms.geocode import *
 from algorithms.spatial_dist_min import spatial_dist_min
 
 # Create a single instance of ImageProcessor
 preprocessor_photo = ImageProcessor("germany_beer_map/data/images/map.jpg")
 preprocessor_ref = ImageProcessor("germany_beer_map/data/images/map_ref.jpg")
-
 
 
 # Use the processed image in both feature detection modules
@@ -35,50 +33,18 @@ ref_contour = resample_contour(ref_contour)
 scale_factor = calculate_scale_factor(contour, ref_contour)
 ref_contour_scaled = scale_contour(ref_contour, scale_factor) # sqrt of scale factor
 
-
-
 ref_contour_scaled_aligned = translate_contour(contour, ref_contour_scaled).reshape(-1, 2)
 
-
-
-
-
 rotation_angle = find_optimal_rotation(ref_contour_scaled_aligned, contour)
-# print(rotation_angle*180/3.16259)
 
 contour_rotated = rotate_contour_around_centroid(contour, -rotation_angle)
 
 circles_coords = two_dim_interp(circles, True,  ref_contour_scaled_aligned)
 
-print(circles_coords)
+bottle_cap_coords = convert_address_to_coords("germany_beer_map/data/mapping/bottle_caps.csv")
 
-
-# Read in bottle_caps.csv
-bottle_caps = []
-with open("germany_beer_map/data/mapping/bottle_caps.csv", 'r') as f:
-	reader = csv.reader(f, delimiter=',')
-	next(reader)  # Skip the header
-	for row in reader:
-		bottle_caps.append(row)
-
-bottle_cap_coords = []
-
-# Get the geocoordinates of each bottle cap
-for i in range(len(bottle_caps)):
-	bottle_cap = get_geocoordinates(bottle_caps[i][2])
-	bottle_cap_coords.append(bottle_cap)
-
-# Convert to numpy array
-np.array(bottle_cap_coords)
-
-# bottle_cap_coords = [ [13.0944453, 54.2907393 ], [12.5440824,  50.68292505], [12.5440824,  50.68292505],
-#  [ 7.95650099, 50.9910225 ],
-#  [ 9.653526,   51.4176975 ],
-#  [11.55363663, 48.14608575],
-#  [11.9069408,  48.3068168 ],
-#  [6.65434605 51.83582915]]
-
-spatial_dist_min(bottle_cap_coords, circles_coords)
+placements, min_dist = spatial_dist_min(bottle_cap_coords, circles_coords, plotting=True)
+print(placements)
 exit(0)
 
 # draw_contours(ref_contour_scaled_aligned)
